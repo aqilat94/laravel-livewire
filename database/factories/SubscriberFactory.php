c333d061e0e0458ccfcba1885b96af3a3ac7041d
aqilat94 <aqilat94@gmail.com>
2022-03-04 20:43:03 +0800

    create subscriber factory

--- database/factories/SubscriberFactory.php
+++ database/factories/SubscriberFactory.php
@@ -17,7 +17,8 @@ class SubscriberFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
         ];
     }
 }
